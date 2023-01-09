e5983e5e3b3dcddd0ecc9b46b5f40efea69c985d
Nivin Joseph <nivinjoseph@outlook.com>
2023-01-09 16:40:14 -0500

    preparing to publish new version

--- dist/infra-config.d.ts
+++ dist/infra-config.d.ts
@@ -2,6 +2,7 @@ import { EnvType } from "./env-type";
 import { Tags } from "@pulumi/aws";
 export declare class InfraConfig {
     private static readonly _pulumiAwsConfig;
+    private static readonly _pulumiAppConfig;
     private static _userTags;
     static get awsAccount(): string;
     static get awsRegion(): string;
@@ -10,4 +11,6 @@ export declare class InfraConfig {
     static get ecr(): string;
     private constructor();
     static configureTags(tags: Record<string, string>): void;
+    static getConfig(key: string): string | null;
+    static requireConfig(key: string): string;
 }
--- dist/infra-config.js
+++ dist/infra-config.js
@@ -2,13 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.InfraConfig = void 0;
 const env_type_1 = require("./env-type");
-const pulumi = require("@pulumi/pulumi");
+const Pulumi = require("@pulumi/pulumi");
 const n_defensive_1 = require("@nivinjoseph/n-defensive");
 class InfraConfig {
     static get awsAccount() { return this._pulumiAwsConfig.require("account"); }
     static get awsRegion() { return this._pulumiAwsConfig.require("region"); }
     static get env() {
-        const env = pulumi.getStack();
+        const env = Pulumi.getStack();
         (0, n_defensive_1.given)(env, "env").ensureHasValue().ensureIsEnum(env_type_1.EnvType);
         return env;
     }
@@ -23,8 +23,16 @@ class InfraConfig {
         (0, n_defensive_1.given)(tags, "tags").ensureHasValue().ensureIsObject();
         this._userTags = tags;
     }
+    static getConfig(key) {
+        var _a;
+        return (_a = this._pulumiAppConfig.get(key)) !== null && _a !== void 0 ? _a : null;
+    }
+    static requireConfig(key) {
+        return this._pulumiAppConfig.require(key);
+    }
 }
 exports.InfraConfig = InfraConfig;
-InfraConfig._pulumiAwsConfig = new pulumi.Config("aws");
+InfraConfig._pulumiAwsConfig = new Pulumi.Config("aws");
+InfraConfig._pulumiAppConfig = new Pulumi.Config("app");
 InfraConfig._userTags = null;
 //# sourceMappingURL=infra-config.js.map
