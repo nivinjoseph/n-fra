552aa81e5a0dabbaa9af9c2dfb64578441ba5b46
Nivin Joseph <nivinjoseph@outlook.com>
2023-03-31 08:59:49 -0400

    preparing to publish new version

--- dist/observability/datadog-integration-config.d.ts
+++ dist/observability/datadog-integration-config.d.ts
@@ -1,5 +1,10 @@
 export interface DatadogIntegrationConfig {
+    ddHost: string;
     apiKey: string;
     appKey: string;
-    notificationsSlackChannel: string;
+    /**
+     * @description this the Slack Account Name (string) inside datadog. Not the Slack Workspace name
+     */
+    slackAccountName: string;
+    slackChannelName: string;
 }
